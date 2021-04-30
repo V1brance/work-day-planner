24cba353d2c163181293abebeb6c788d73c683ec
Neon Riot <sbrookshire99@gmail.com>
2021-04-29 21:35:17 -0500

    Added all timeslots, moment library utilized, page now shows timeblocks with proper class for past, present, future

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,40 @@
-$(".saveBtn").on("click", function () {
-  var userInput = $("#event-area").text;
-  console.log(userInput);
+$(".saveBtn").on("click", function (event) {
+  var whichTime = $(event.target).attr("id");
+  var inputSelector = whichTime + "event-area";
+  var inputEvent = $("#" + inputSelector).val();
+  var savedEvent = localStorage.setItem(inputSelector, inputEvent);
+  refreshElements();
 });
+
+function refreshElements() {
+  $("input").each(function () {
+    var inputArea = $(this).attr("id");
+    console.log(inputArea);
+    var savedText = localStorage.getItem(inputArea);
+    $(this).attr("placeholder", savedText);
+    updateTime(inputArea);
+  });
+}
+
+function updateTime(timeSlot) {
+  var time = moment();
+  slotTime = parseInt(timeSlot.split("-")[0]);
+  if (slotTime > 6) {
+    slotTime += " am";
+  } else {
+    slotTime += " pm";
+  }
+  var slotMoment = moment(slotTime, "h a");
+  var timeDiff = time.diff(slotMoment, "hours");
+  var otherClasses = " form-control h-100";
+  if (timeDiff > 0) {
+    $("#" + timeSlot).attr("class", "past" + otherClasses);
+    $("#" + timeSlot).prop("disabled", true);
+  } else if (timeDiff < 0) {
+    $("#" + timeSlot).attr("class", "future" + otherClasses);
+  } else {
+    $("#" + timeSlot).attr("class", "present" + otherClasses);
+  }
+}
+
+refreshElements();
